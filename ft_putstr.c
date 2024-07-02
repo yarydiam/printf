ca2447a3d3018056e680c22a2a0f95716a045142
yara <yarysalinas@gmail.com>
2024-07-02 21:03:11 +0200

    printf

--- ft_putstr.c
+++ ft_putstr.c
@@ -12,18 +12,17 @@
 
 #include "ft_printf.h"
 
-int ft_putstr(char *str)
+int	ft_putstr(char *str)
 {
-    int i;
+	int i;
     i = 0;
 
-    if (!str)
-        return(ft_putstr("(null)"));
-
-    while (str[i])
-    {
-        ft_putchar(str[i]);
-        i++;
-    }
-    return(i);
+	if (!str)
+		return(ft_putstr("(null)"));
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	return(i);
 }
