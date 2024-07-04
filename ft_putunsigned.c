d61af8e340a72be2481074f5a16959767b847364
yara <yarysalinas@gmail.com>
2024-07-04 12:51:48 +0200

    printf

---
+++ ft_putunsigned.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putunsigned.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yadiaman <yadiaman@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 12:36:54 by yadiaman          #+#    #+#             */
+/*   Updated: 2024/07/04 12:36:54 by yadiaman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include "ft_printf.h"
+
+int	ft_putunsigned(unsigned int num)
+{
+	int	i;
+
+	i = 0;
+	if (num > 9)
+	{
+		i += ft_putunsigned(num /10);
+		i += ft_putchar(num % 10 + '0');
+	}
+	else
+		i += ft_putchar(num + '0');
+	return (i);
+}
