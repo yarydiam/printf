84dd45dadacdc02aa814d36bb8e632ae72abd609
Yara Diamantino Salinas <yadiaman@c3r1s5.42malaga.com>
2024-07-08 13:55:21 +0200

    print

---
+++ ft_putptr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putptr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yadiaman <yadiaman@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:46:54 by yadiaman          #+#    #+#             */
+/*   Updated: 2024/07/08 12:53:09 by yadiaman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putptr(void *n)
+{
+	int						i;
+	unsigned long long int	ptr;
+
+	i = 0;
+	if (!n)
+		return (ft_putstr("(nil)"));
+	ptr = (unsigned long long int)n;
+	ft_putstr("0x");
+	i = ft_puthexa(ptr, "0123456789abcdef");
+	return (i + 2);
+}
